e115f13f288b28a992dcf2fb068092096ce80d2a
alanpicarrot <alanpicarrot@gmail.com>
2017-07-22 10:56:05 +0800

    Correct Code

--- ReverseArray/main.cpp
+++ ReverseArray/main.cpp
@@ -3,28 +3,36 @@ using namespace std;
 
 int* reverse (const int* list, int size)
 {
-    int result [6];
+    int* result = new int[size] ;
+    
     for ( int i = 0 , j = size - 1 ; i < size; i++, j-- )
     {
         result[j] = list[i];
     }
+    
     return result;
+    
 }
 
 void printArray(const int* list, int size)
 {
     int i;
+    
     for(i = 0; i < size; i++)
-    {
         
         cout << list[i] << " " ;
-    }
+    
+    cout << endl ;
+    
 }
 
-int main() {
-    int list[] = {1, 2, 3, 4, 5, 6};
+int main()
+{
+    int list[] = {1, 2, 3, 4, 5, 6}; // new int[size] 即為這個整數陣列配置適當大小的記憶體。
+    
     int* p = reverse(list, 6);
-    printArray(p,6);
+    
+    printArray(p, 6);
     
     return 0;
 }
